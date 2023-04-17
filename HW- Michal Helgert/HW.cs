58256285dd266e8a1f5c27dc51787f5aa187aeb2
Jan Gergel <jan.gergel@educhem.cz>
2023-04-17 08:53:35 +0200

    Přidejte soubory projektu.

---
+++ HW- Michal Helgert/HW.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Educhem
+
+{
+    public static partial class HW
+    {
+
+        
+        public static double Propocitej (double nabijeciproud, double kapacita)
+        {
+
+            return kapacita / (nabijeciproud * 1.4);
+
+           
+            
+            
+        }
+        
+
+        
+         
+    }
+}
